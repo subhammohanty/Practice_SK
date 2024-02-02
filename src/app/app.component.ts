4d6f8ca915e5c6c863d52e071e00bcc980fc7a0c
smohant1 <subham.mohanty@capgemini.com>
2024-02-02 10:32:01 +0530

    First

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  providers: [MessageService]
+})
+export class AppComponent {
+}
